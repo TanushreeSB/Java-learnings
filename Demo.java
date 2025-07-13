f9784265a442bbcd781043da7ecf018dd83b7d77
Tanushree Borase <tanushreebcs@gmail.com>
2025-07-13 20:36:55 +0530

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,5 +1,14 @@
 public class Demo { // name of file is Demo.java implies that this Demo class will be public
     public static void main(String[] args) { // String passed as argument
-        System.out.println(args[1]);
+        System.out.println(args[1]); // println for new line
+        System.out.print("Hello Java");
     }
 }
+
+/*
+
+output:
+Hello and welcome!
+Hello Java
+
+*/
