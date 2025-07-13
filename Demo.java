66714a3b90708acd13da154c914aac37d031ebc2
Tanushree Borase <tanushreebcs@gmail.com>
2025-07-13 20:07:15 +0530

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,5 @@
+public class Demo { // name of file is Demo.java implies that this Demo class will be public
+    public static void main(String[] args) {
+        System.out.println(args[1]);
+    }
+}
