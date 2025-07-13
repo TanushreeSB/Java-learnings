8e3ac163baaba1342c01ebebf9096fb0d34750d9
Tanushree Borase <tanushreebcs@gmail.com>
2025-07-13 21:34:53 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);  // To take input
+        System.out.println(sc.nextLine());
+    }
+}
+
+/*
+output:
+hello tanushree coder
+hello tanushree coder
+*/
+
+*******************************************************************************************************************************************************************************************************************************
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);  // To take input
+        System.out.println(sc.next());
+    }
+}
+
+/*
+output:
+tanu perform
+tanu
+*/
