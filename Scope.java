54cb5441c5e0dafaa666e1403703298c7f8f1924
Tanushree Borase <tanushreebcs@gmail.com>
2025-07-14 16:04:14 +0530

    Create Scope.java

---
+++ Scope.java
@@ -0,0 +1,22 @@
+public class Scope {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        {
+            int a = 78; // already initialised outside the block in the same
+            a = 100; // reassign the origin ref variable to some other value
+            System.out.println(a);
+            int c = 99;
+            // values initialised in this block, will remain in block
+        }
+        System.out.println(a);
+        // System.out.println(c); // cannot use outside the block
+    }
+
+    static void random(int marks) {
+        int num = 67;
+        System.out.println(num);
+        System.out.println(marks);
+    }
+}
