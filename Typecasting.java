2ade85b453213045ef485e764361ef537bb6d29e
Tanushree Borase <tanushreebcs@gmail.com>
2025-07-14 10:19:16 +0530

    Update Typecasting.java

--- Typecasting.java
+++ Typecasting.java
@@ -5,10 +5,20 @@ public class TypeCasting {
         Scanner input = new Scanner(System.in);
         int num = (int)(56.78f);
         System.out.println(num);
+
+        int a = 257;
+        byte b = (byte)(a);
+
+        System.out.println(b); // byte size is till 256, so to print we get remainder 257- 256 = 1
+
+        int number = 'A';
+        System.out.println(number);
     }
 }
 
 /* 
 output:
 56
+1
+65
 */
