eb95b563522a324d5d8ef798fa354162ae59067b
Tanushree Borase <tanushreebcs@gmail.com>
2025-07-14 08:50:08 +0530

    Update and rename INputs.java to Next.java

--- Next.java
+++ Next.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Inputs {
+public class Next {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Please enter some input: ");
