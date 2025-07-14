924d90200fb71f47b738852b94f50c884d481399
Tanushree Borase <tanushreebcs@gmail.com>
2025-07-14 09:38:40 +0530

    Create Typecasting.java

---
+++ Typecasting.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = (int)(56.78f);
+        System.out.println(num);
+    }
+}
+
+/* 
+output:
+56
+*/
