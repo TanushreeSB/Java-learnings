3a03d0400929ca4be50ff78e853eedfce912f279
Tanushree Borase <tanushreebcs@gmail.com>
2025-08-08 16:19:35 +0530

    Create object.java

---
+++ OOPS/object.java
@@ -0,0 +1,18 @@
+public class object {
+  int x = 5;
+
+  public static void main(String[] args) {
+    Main myObj = new Main();
+    System.out.println(myObj.x);
+  }
+}
+
+/*
+Create an Object
+In Java, an object is created from a class. We have already created the class named Main, so now we can use this to create objects.
+
+To create an object of Main, specify the class name, followed by the object name, and use the keyword new:
+
+Example in code:
+Create an object called "myObj" and print the value of x:
+*/
