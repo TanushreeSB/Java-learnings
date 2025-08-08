cf5275faa09d196dadd876a4e1c3d2c2a3ba6fed
Tanushree Borase <tanushreebcs@gmail.com>
2025-08-08 16:20:52 +0530

    Update object.java

--- OOPS/object.java
+++ OOPS/object.java
@@ -2,7 +2,7 @@ public class Object {
   int x = 5;
 
   public static void main(String[] args) {
-    Object myObj = new Main();
+    Object myObj = new Object();
     System.out.println(myObj.x);
   }
 }
