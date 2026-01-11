e9662dc7e204c88b9b2f41059bae487f97e24e29
Tanushree Borase <tanushreebcs@gmail.com>
2026-01-11 11:32:25 +0530

    Update hello.java

--- java basics/hello.java
+++ java basics/hello.java
@@ -3,3 +3,7 @@ class Main {
         System.out.println("hello java");
     }
 }
+
+/*
+Every line of code that runs in Java must be inside a class. The class name should always start with an uppercase first letter. In our example, we named the class Main.
+*/
