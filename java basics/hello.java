89536551139ca0b363d32aaea18f59c4164e9d38
Tanushree Borase <tanushreebcs@gmail.com>
2026-01-12 11:14:38 +0530

    Update hello.java

--- java basics/hello.java
+++ java basics/hello.java
@@ -6,4 +6,9 @@ class Main {
 
 /*
 Every line of code that runs in Java must be inside a class. The class name should always start with an uppercase first letter. In our example, we named the class Main.
+
+System is a built-in Java class.
+out is a member of System, short for "output".
+println() is a method, short for "print line".
+Finally, remember that each Java statement must end with a semicolon (;).
 */
