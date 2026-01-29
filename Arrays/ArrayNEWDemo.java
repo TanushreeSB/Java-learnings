b2cd2121ed2679b29e936ddfbf5cd4e6a585cc22
Tanushree Borase <tanushreebcs@gmail.com>
2026-01-29 15:40:00 +0530

    Create ArrayNEWDemo.java

---
+++ Arrays/ArrayNEWDemo.java
@@ -0,0 +1,18 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class ArrayNEWDemo {
+    public static void main(String[] args) {
+      String[] cars = new String[4]; // size is 4
+
+    cars[0] = "Volvo";
+    cars[1] = "BMW";
+    cars[2] = "Ford";
+    cars[3] = "Mazda";
+    
+    System.out.println(cars[0]);  
+    }
+}
+
+// Volvo
+// We can also create an array by specifying its size with new. This makes an empty array with space for a fixed number of elements, 
