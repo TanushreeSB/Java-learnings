06bf4f1dff8b368c3e3615a1e0d4ac39d5c148d5
Tanushree Borase <tanushreebcs@gmail.com>
2026-01-29 14:32:07 +0530

    Create ArayDemo.java

---
+++ Arrays/ArayDemo.java
@@ -0,0 +1,12 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class ArrayDemo {
+    public static void main(String[] args) {
+       String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+       cars[0] = "Opel";
+       System.out.println(cars[0]);  
+    }
+}
+
+// outputs Opel instead of Volvo
