707120a3fb396f112acbc240ab0f58ab723058be
Tanushree Borase <tanushreebcs@gmail.com>
2026-02-20 11:46:24 +0530

    Create ModifyAttributeDemo.java

---
+++ OOPS/ModifyAttributeDemo.java
@@ -0,0 +1,10 @@
+public class ModifyAttributeDemo {
+  int x; // In Java, variables declared inside a class are called "attributes".
+
+  public static void main(String[] args) {
+    ModifyAttributeDemo myObj = new ModifyAttributeDemo();
+    myObj.x = 40;
+    System.out.println(myObj.x); // 40 , You can access attributes by creating an object of the class, and by using the dot syntax (.)
+  }
+}
+
