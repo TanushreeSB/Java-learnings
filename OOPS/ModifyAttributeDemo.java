6cf8d58d03ecb8440891043409d4d01614359062
Tanushree Borase <tanushreebcs@gmail.com>
2026-02-20 11:48:10 +0530

    Update ModifyAttributeDemo.java

--- OOPS/ModifyAttributeDemo.java
+++ OOPS/ModifyAttributeDemo.java
@@ -8,3 +8,18 @@ public class ModifyAttributeDemo {
   }
 }
 
+---------------------------------------------------------------------------------
+
+// If you don't want the ability to override existing values, declare the attribute as final:
+/*
+Example
+public class Main {
+  final int x = 10;
+
+  public static void main(String[] args) {
+    Main myObj = new Main();
+    myObj.x = 25; // will generate an error: cannot assign a value to a final variable
+    System.out.println(myObj.x);
+  }
+}
+*/
