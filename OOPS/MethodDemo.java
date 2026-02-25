196521c606e3176bbf359c4b0db35d031f8206c5
Tanushree Borase <tanushreebcs@gmail.com>
2026-02-25 12:06:28 +0530

    Update MethodDemo.java

--- OOPS/MethodDemo.java
+++ OOPS/MethodDemo.java
@@ -18,3 +18,28 @@ static means the method belongs to the class, not to an object.
 So you can call it without creating an object.
 
 */
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+public class MethodDemo {
+    public void MyMethod(){
+        System.out.println("I love Java");
+    }
+
+    
+    public static void main(String[] args) {
+        Main obj = new Main();
+        obj.MyMethod();
+    }
+}
+
+/*
+
+Access Methods With an Object
+This method is non-static.
+Non-static methods belong to an object, not the class.
+
+*/
