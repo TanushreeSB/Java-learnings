0efd4947c47206a10eaeb59c5ee54a02445cb074
Tanushree Borase <tanushreebcs@gmail.com>
2026-05-07 12:28:36 +0530

    Create defaultValues.java

---
+++ java basics/defaultValues.java
@@ -0,0 +1,25 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class defaultValues {
+    
+        static short a;
+        static byte b;
+        static int i;
+        static char c;
+        static float f;
+        static long l;
+        static double d;
+        static boolean bo;
+        
+        public static void main(String[] args) {
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(i);
+            System.out.println(c);
+            System.out.println(f);
+            System.out.println(l);
+            System.out.println(d);
+            System.out.println(bo);
+    }
+}
