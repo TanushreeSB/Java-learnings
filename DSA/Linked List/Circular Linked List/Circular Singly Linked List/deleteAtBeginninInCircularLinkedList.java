60ca2467ec79915e3f36c4cb925f483314baa95a
Tanushree Borase <tanushreebcs@gmail.com>
2026-05-15 12:21:19 +0530

    Update deleteAtBeginninInCircularLinkedList.java

--- DSA/Linked List/Circular Linked List/Circular Singly Linked List/deleteAtBeginninInCircularLinkedList.java
+++ DSA/Linked List/Circular Linked List/Circular Singly Linked List/deleteAtBeginninInCircularLinkedList.java
@@ -68,3 +68,101 @@ class deleteAtBeginninInCircularLinkedList {
         cll.display();
     }
 }
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+class CircularLinkedList {
+
+    Node head;
+
+    void insert(int data) {
+// at end insertion
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            newNode.next = head;
+            return;
+        }
+
+        Node temp = head;
+
+        while (temp.next != head) {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+        newNode.next = head;
+    }
+
+    void deleteAtBeginning() {
+
+        if (head == null) return;
+
+        if (head.next == head) {
+            head = null;
+            return;
+        }
+
+        Node temp = head;
+
+        while (temp.next != head) {
+            temp = temp.next;
+        }
+
+        temp.next = head.next;
+        head = head.next;
+    }
+
+    void display() {
+
+        if (head == null) {
+            System.out.println("Empty list");
+            return;
+        }
+
+        Node temp = head;
+
+        do {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        } while (temp != head);
+
+        System.out.println("(back to head)");
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        CircularLinkedList cll = new CircularLinkedList();
+
+        System.out.print("Enter number of nodes: ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter data: ");
+            cll.insert(sc.nextInt());
+        }
+
+        System.out.println("Before deletion:");
+        cll.display();
+
+        cll.deleteAtBeginning();
+
+        System.out.println("After deletion at beginning:");
+        cll.display();
+
+        sc.close();
+    }
+}
