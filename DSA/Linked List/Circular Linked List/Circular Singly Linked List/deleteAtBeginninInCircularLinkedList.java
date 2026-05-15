34504f1a4f62a1fe8f30d8bf30caf313033752f9
Tanushree Borase <tanushreebcs@gmail.com>
2026-05-15 12:16:22 +0530

    Create deleteAtBeginninInCircularLinkedList.java

---
+++ DSA/Linked List/Circular Linked List/Circular Singly Linked List/deleteAtBeginninInCircularLinkedList.java
@@ -0,0 +1,70 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+class deleteAtBeginninInCircularLinkedList {
+
+    Node head;
+
+    void deleteAtBeginning() {
+
+        if (head == null) return;
+
+        if (head.next == head) {
+            head = null;
+            return;
+        }
+
+        Node temp = head;
+
+        while (temp.next != head) {
+            temp = temp.next;
+        }
+
+        temp.next = head.next;
+        head = head.next;
+    }
+
+    void display() {
+
+        if (head == null) {
+            System.out.println("Empty");
+            return;
+        }
+
+        Node temp = head;
+
+        do {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        } while (temp != head);
+
+        System.out.println("(back to head)");
+    }
+
+    public static void main(String[] args) {
+
+        CircularLinkedList cll = new CircularLinkedList();
+
+        cll.head = new Node(10);
+        Node second = new Node(20);
+        Node third = new Node(30);
+
+        cll.head.next = second;
+        second.next = third;
+        third.next = cll.head;
+
+        System.out.println("Before deletion:");
+        cll.display();
+
+        cll.deleteAtBeginning();
+
+        System.out.println("After deletion at beginning:");
+        cll.display();
+    }
+}
