5f6aa51211b4181dd712882c0b27fa199af5b4e7
Tanushree Borase <tanushreebcs@gmail.com>
2026-05-24 19:57:15 +0530

    Create FloorSolution.java

---
+++ Binary Search/FloorSolution.java
@@ -0,0 +1,49 @@
+// Floor means: Greatest element ≤ x
+
+class FloorSolution {
+    public int findFloor(int[] arr, int x) {
+
+        int ans = -1;
+
+        for(int i = 0; i < arr.length; i++) {
+
+            if(arr[i] <= x) {
+                ans = i;
+            }
+            else {
+                break;
+            }
+        }
+
+        return ans;
+    }
+}
+
+// TC → O(n) , SC → O(1)
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class Solution {
+    public int findFloor(int[] arr, int x) {
+
+        int start = 0, end = arr.length - 1;
+        int ans = -1;
+
+        while(start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+            if(arr[mid] <= x) {
+                ans = mid;
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+}
+
+// TC → O(log n), SC → O(1)
