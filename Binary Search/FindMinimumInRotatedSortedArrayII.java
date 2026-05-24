32a82e8455af66d03941b7f3f6858de0cb6ddf5b
Tanushree Borase <tanushreebcs@gmail.com>
2026-05-24 18:08:06 +0530

    Update FindMinimumInRotatedSortedArrayII.java

--- Binary Search/FindMinimumInRotatedSortedArrayII.java
+++ Binary Search/FindMinimumInRotatedSortedArrayII.java
@@ -25,7 +25,21 @@ If the right half is sorted (nums[mid] < nums[end]), the minimum must be in the 
 
 If the left half is sorted (nums[mid] > nums[end]), the minimum must be in the right half.
 
-If duplicates block the decision (nums[mid] == nums[end]), we safely reduce end by one, since nums[end] cannot be the unique minimum.
+If duplicates block the decision (nums[mid] == nums[end]), we safely reduce end by one.
+But if nums[mid] == nums[end], we cannot decide which side contains the minimum.
+
+Example:
+
+Code
+nums = [2, 2, 2, 0, 1]
+At some point, mid and end both point to 2.
+
+The minimum could still be to the left (where 0 is).
+
+Or it could be at end itself.
+Since we can’t tell, we safely shrink the search space by doing end--.
+
+This way, we don’t lose the minimum, but we reduce the ambiguity caused by duplicates.
 
 Complexity
 Time: Worst case 
