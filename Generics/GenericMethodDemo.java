06664aa999b20e3011123fe71a5808415890098d
Tanushree Borase <tanushreebcs@gmail.com>
2026-05-29 09:31:22 +0530

    Create GenericMethodDemo.java

---
+++ Generics/GenericMethodDemo.java
@@ -0,0 +1,28 @@
+class Main {
+    // Generic method: <T> declares type parameter, T is return type
+    public <T> T getValue(T data) {
+        return data;
+    }
+}
+
+class GenericMethodDemo {
+    public static void main(String[] args) {
+        Demo demo = new Demo();
+
+        // Using with String
+        String str = demo.getValue("Hello Generics");
+        System.out.println(str);
+
+        // Using with Integer
+        Integer num = demo.getValue(123);
+        System.out.println(num);
+
+        // Using with Double
+        Double d = demo.getValue(45.67);
+        System.out.println(d);
+
+        // Using with Boolean
+        Boolean b = demo.getValue(true);
+        System.out.println(b);
+    }
+}
