7f6f21bf324045d1aa2fd74eb7d567de45ebc96b
Tanushree Borase <tanushreebcs@gmail.com>
2026-09-13 15:05:52 +0530

    Create Greater_Than_Neighbors.java

---
+++ Arrays/Practice questions/Greater_Than_Neighbors.java
@@ -0,0 +1,15 @@
+class Greater_Than_Neighbors {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 5, 2, 7, 3, 6, 4};
+
+        for (int i = 1; i < arr.length - 1; i++) {
+
+            if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
+
+/*
